d50389bd2ab63b5e8d9f62249bdfd78cb5b323eb
obitomrdevapi-code <obitomrdevapi@gmail.com>
2025-10-12 16:13:28 +0100

    Update and rename akinator_answer to akinator_answer.js

--- api/akinator_answer.js
+++ api/akinator_answer.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+    return res.status(405).json({ error: "المرجو ملئ جميع الحقول" });
   }
 
   try {
